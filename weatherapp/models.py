083c31bff9cc34048be55fadc87179e1f9c26d8a
Dev <dev@192.168.1.11>
2024-07-15 06:35:23 -0700

    Retrieve City Data

--- weatherapp/models.py
+++ weatherapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=25)
+    def _str_(self): #show the actual city name on the dashboard
+        return self.name
--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+import requests
+from .models import City
 
 # Create your views here.
 def index(request):
-    return render(request, 'weatherapp/index.html')
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=2422aa458f371a6f8d8e8b9a2d7a1af8'
+    cities = City.objects.all()
+    weather_data = []
+    for city in cities:
+        city_weather = requests.get(url.format(city.name)).json() #request the API data and convert the JSON to Python data types
+        weather = {
+            'city' : city.name,
+            'temperature' : city_weather['main']['temp'],
+            'description' : city_weather['weather'][0]['description'],
+            'icon' : city_weather['weather'][0]['icon']
+        }
+        weather_data.append(weather)
+    context = {'weather_data' : weather_data}
+    return render(request, 'weatherapp/index.html', context)
